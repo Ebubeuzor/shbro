1cd245beefe030c8305ab9a91e07893519f9a1c4
Ebube Uzor <ebubeuzor17@gmail.com>
2024-01-31 23:21:33 +0100

    fixing all bug

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -30,23 +30,23 @@ class Kernel extends ConsoleKernel
         // $schedule->job(new ProcessEmailReminders)->everyMinute();
         // $schedule->job(new ClearRouteCacheJob)->everyMinute();
 
-        // $bookings = Booking::where('paymentStatus','success')->get(); 
+        $bookings = Booking::where('paymentStatus','success')->get(); 
 
-        // foreach ($bookings as $booking) {
+        foreach ($bookings as $booking) {
             
-        //     // Schedule the TwoDayReminderJob two days before the check-in date
-        //     $schedule->job(new TwoDayReminderJob($booking))->everyMinute();
+            // Schedule the TwoDayReminderJob two days before the check-in date
+            $schedule->job(new TwoDayReminderJob($booking))->everyMinute();
     
-        //     // Schedule the FewHoursReminderJob a few hours before the check-in time
-        //     $schedule->job(new FewHoursReminderJob($booking))->everyMinute();
+            // Schedule the FewHoursReminderJob a few hours before the check-in time
+            $schedule->job(new FewHoursReminderJob($booking))->everyMinute();
     
-        //     // Schedule the CheckInNotificationJob at the check-in time
-        //     $schedule->job(new CheckInNotificationJob($booking))->everyMinute();
+            // Schedule the CheckInNotificationJob at the check-in time
+            $schedule->job(new CheckInNotificationJob($booking))->everyMinute();
             
-        //     // Schedule the CheckInNotificationJob at the check-in time
-        //     $schedule->job(new CheckOutNotificationJob($booking))->everyMinute();
+            // Schedule the CheckInNotificationJob at the check-in time
+            $schedule->job(new CheckOutNotificationJob($booking))->everyMinute();
             
-        // }       
+        }       
     }
 
     /**

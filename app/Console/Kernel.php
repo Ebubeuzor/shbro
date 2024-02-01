f1a59ebacdd5cf21ef3b4382619a2a505dbc9443
Ebube Uzor <ebubeuzor17@gmail.com>
2024-02-01 12:28:40 +0100

    fixing all bug

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,24 +27,24 @@ class Kernel extends ConsoleKernel
     //     Log::info('Testing scheduler. Current time: ' . now());
     // })->everyMinute();
 
-    $schedule->job(new ProcessEmailReminders)->daily(); // Change to everyFiveMinutes
-    $schedule->job(new ClearRouteCacheJob)->daily();     // Change to everyTenMinutes
+    // $schedule->job(new ProcessEmailReminders)->daily(); // Change to everyFiveMinutes
+    $schedule->job(new ClearRouteCacheJob)->everyMinute();     // Change to everyTenMinutes
 
-    $bookings = Booking::where('paymentStatus','success')->get(); 
+    // $bookings = Booking::where('paymentStatus','success')->get(); 
 
-    foreach ($bookings as $booking) {
+    // foreach ($bookings as $booking) {
         // Schedule the TwoDayReminderJob two days before the check-in date
-        $schedule->job(new TwoDayReminderJob($booking))->everyFourMinutes();
+        // $schedule->job(new TwoDayReminderJob($booking))->everyMinute();
 
-        // Schedule the FewHoursReminderJob a few hours before the check-in time
-        $schedule->job(new FewHoursReminderJob($booking))->everyTwoMinutes(); // Change to everyTwoMinutes
+        // // Schedule the FewHoursReminderJob a few hours before the check-in time
+        // $schedule->job(new FewHoursReminderJob($booking))->everyMinute(); // Change to everyTwoMinutes
+
+        // // Schedule the CheckInNotificationJob at the check-in time
+        // $schedule->job(new CheckInNotificationJob($booking))->everyMinute();
 
         // Schedule the CheckInNotificationJob at the check-in time
-        $schedule->job(new CheckInNotificationJob($booking))->everyFiveMinutes();
-
-        // Schedule the CheckInNotificationJob at the check-in time
-        $schedule->job(new CheckOutNotificationJob($booking))->everyTenMinutes();
-    }       
+        // $schedule->job(new CheckOutNotificationJob($booking))->everyMinute();
+    // }       
 }
 
 

28a6c797723c8b052610efa4c6ccc5c054557042
Ebube Uzor <ebubeuzor17@gmail.com>
2024-02-05 07:42:36 +0100

    fixed all bug

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,36 +14,38 @@ return new class extends Migration
 
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique()->nullable();
-            $table->string('password')->nullable();
-            $table->string('phone')->nullable();
-            $table->string('verification_type')->nullable();
-            $table->string('government_id')->nullable();
-            $table->string('live_photo')->nullable();
-            $table->string('profilePicture')->nullable();
-            $table->string('country')->nullable();
-            $table->string('street')->nullable();
-            $table->string('zipcode')->nullable();
-            $table->string('state')->nullable();
-            $table->string('city')->nullable();
-            $table->integer('host')->default(0);
-            $table->string('verified')->nullable();
-            $table->unsignedInteger('delete_attempts')->default(0);
-            $table->boolean('is_active')->default(true);
-            $table->string('adminStatus')->nullable();
-            $table->timestamp('last_login_at')->nullable();
-            $table->boolean('email_verified_at')->default(0);
-            $table->string('emergency_no')->nullable();
-            $table->string('google_id')->nullable();
-            $table->rememberToken();
-            $table->timestamps();
-            $table->softDeletes();
-            $table->string('banned')->nullable();
-            $table->string('suspend')->nullable();
-        });
+        if (!Schema::hasTable('users')) {
+            Schema::create('users', function (Blueprint $table) {
+                $table->id();
+                $table->string('name');
+                $table->string('email')->unique()->nullable();
+                $table->string('password')->nullable();
+                $table->string('phone')->nullable();
+                $table->string('verification_type')->nullable();
+                $table->string('government_id')->nullable();
+                $table->string('live_photo')->nullable();
+                $table->string('profilePicture')->nullable();
+                $table->string('country')->nullable();
+                $table->string('street')->nullable();
+                $table->string('zipcode')->nullable();
+                $table->string('state')->nullable();
+                $table->string('city')->nullable();
+                $table->integer('host')->default(0);
+                $table->string('verified')->nullable();
+                $table->unsignedInteger('delete_attempts')->default(0);
+                $table->boolean('is_active')->default(true);
+                $table->string('adminStatus')->nullable();
+                $table->timestamp('last_login_at')->nullable();
+                $table->boolean('email_verified_at')->default(0);
+                $table->string('emergency_no')->nullable();
+                $table->string('google_id')->nullable();
+                $table->rememberToken();
+                $table->timestamps();
+                $table->softDeletes();
+                $table->string('banned')->nullable();
+                $table->string('suspend')->nullable();
+            });
+        }
     }
 
     /**

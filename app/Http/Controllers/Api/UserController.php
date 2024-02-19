bd615c3086b999566633f99e712fa2ab51989f76
Ebube Uzor <ebubeuzor17@gmail.com>
2024-02-19 14:37:16 +0100

    fixing all errors

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -1297,19 +1297,17 @@ class UserController extends Controller
                     ->where('paymentStatus', 'success')
                     ->where('hostId', auth()->id())
                     ->where(function ($query) {
-                            $query->where('host_homes.check_in_time', '<', Carbon::now()->format('g:i A'))
-                                ->orWhere(function ($q) {
-                                    // If the check_in_time is '12:00 PM', treat it as '12:00 AM'
-                                    $q->where('host_homes.check_in_time', '12:00 PM')
-                                        ->where('host_homes.check_in_time', '<', Carbon::now()->format('g:i A'));
-                                });
-                        })->get();
+                        $query->where('host_homes.check_in_time', '<', Carbon::now()->format('g:i A'))
+                            ->orWhere('host_homes.check_in_time', '>', Carbon::now()->format('g:i A'));
+                    })
+                    ->get();
 
         // Transform the bookings into the BookedResource
         $bookingsResource = BookedResource::collection($bookings);
 
         return response(['bookings' => $bookingsResource]);
     }
+
 
     /**
      * @lrd:start

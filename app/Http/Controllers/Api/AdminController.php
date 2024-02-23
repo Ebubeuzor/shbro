6591fb29a3113dfa8040a3443dd552f47ecc360e
Ebube Uzor <ebubeuzor17@gmail.com>
2024-02-23 22:07:55 +0100

    fixing all errors

--- app/Http/Controllers/Api/AdminController.php
+++ app/Http/Controllers/Api/AdminController.php
@@ -46,7 +46,7 @@ class AdminController extends Controller
     public function cancelledTrips() {
 
         return CancelTripsResource::collection(
-            Canceltrip::all()
+            Canceltrip::latest()->get()
         );
 
     }

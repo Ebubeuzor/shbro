8cf17a7efd6f81f61eaddb7553965c60b64f3e64
Ebube Uzor <ebubeuzor17@gmail.com>
2024-03-16 06:21:45 +0100

    Update allowed origins to include 'http://127.0.0.1:5500'

--- config/cors.php
+++ config/cors.php
@@ -19,9 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
-
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['http://localhost:5173','https://shbro.onrender.com','http://127.0.0.1:5500', '127.0.0.1:5500'],
 
     'allowed_origins_patterns' => [],
 

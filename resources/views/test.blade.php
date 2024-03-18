d50633d3177887c3d6938efabd296957653a8c21
Ebube Uzor <ebubeuzor17@gmail.com>
2024-03-18 15:43:09 +0100

    Remove `CorsMiddleware` and use Laravel's built-in `HandleCors` middleware instead.

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -25,6 +25,9 @@
 
             window.Echo.connector.options.auth.headers.Authorization = `Bearer 26|bDKqrICjIUHHzQUs04YkWdwBpLMyBQ3tCit4aTyT5849c379`;
 
+            // Log the URL of the broadcasting/auth endpoint
+            const broadcastingAuthUrl = window.Echo.connector.options.authEndpoint;
+            console.log('Broadcasting Auth URL:', broadcastingAuthUrl);
             if (window.Echo.connector.options.auth.headers.Authorization) {
                 console.log('Authentication token is set:', window.Echo.connector.options.auth.headers.Authorization);
                 const privateChannel = window.Echo.private(channelName);

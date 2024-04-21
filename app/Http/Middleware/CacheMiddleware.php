ba36104b1b904b1a56de15fe587d17421216717d
Ebube Uzor <ebubeuzor17@gmail.com>
2024-04-21 18:18:12 +0100

    Cache middleware for GET requests and update api routes
    - Added a condition to cache only GET requests in the CacheMiddleware
    - Updated the api.php routes file to include the 'cache' middleware for all GET requests

--- app/Http/Middleware/CacheMiddleware.php
+++ app/Http/Middleware/CacheMiddleware.php
@@ -17,19 +17,24 @@ class CacheMiddleware
      */
     public function handle(Request $request, Closure $next)
     {
-        // Generate a unique cache key based on the request URL
-        $cacheKey = 'route_' . str_replace('/', '_', $request->path());
+        // Cache only GET requests
+        if ($request->isMethod('GET')) {
+            // Generate a unique cache key based on the request URL
+            $cacheKey = 'route_' . str_replace('/', '_', $request->path());
 
-        if (Cache::has($cacheKey)) {
-            // If the response is cached, return it directly
-            return response(Cache::get($cacheKey));
+            if (Cache::has($cacheKey)) {
+                // If the response is cached, return it directly
+                return response(Cache::get($cacheKey));
+            }
         }
 
         // Proceed with the request and get the response
         $response = $next($request);
 
-        // Cache the response for a certain duration (e.g., 1 hour)
-        Cache::put($cacheKey, $response->getContent(), 604800);
+        // Cache the response for a certain duration for GET requests
+        if ($request->isMethod('GET')) {
+            Cache::put($cacheKey, $response->getContent(), 604800);
+        }
 
         return $response;
     }
--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,8 @@ use Illuminate\Support\Facades\Cookie;
 use Illuminate\Support\Facades\Route;
 
 
+Route::middleware('cache')->group(function () {
+
     Route::get('/api/token/{userId}',[UserController::class, 'getUserToken']);
     Route::post('admin-guest-chat/startConversationOrReplyText', [AdminGuestChatController::class, 'startConversation']);
 
@@ -259,3 +261,4 @@ use Illuminate\Support\Facades\Route;
     Route::get('hosthomesForUnAuthUser', [HostHomeController::class, 'index']);
 
     Route::get('/searchHomeByProperty_typeForUnAuthUser/{property_type}', [HostHomeController::class, 'searchHomeByProperty_type']);
+});

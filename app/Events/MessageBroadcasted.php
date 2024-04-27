f4c19c2b1ba47b14b1453e8d31ea4a1566ab5d38
Ebube Uzor <ebubeuzor17@gmail.com>
2024-04-27 11:08:00 +0100

    done

--- app/Events/MessageBroadcasted.php
+++ app/Events/MessageBroadcasted.php
@@ -62,16 +62,29 @@ class MessageBroadcasted implements ShouldBroadcast
             return ['unattended_chats' => $unattendedChats];
         }else {
             if ($this->userStatus == "guest" || $this->userStatus == "cohost" || $this->userStatus == "host") {
-                return [
-                    'user_id' => $this->user->id,
-                    'message' => $this->message,
-                    'image' => $this->image != null ? $this->image : null,
-                    'admin_id' => $this->receiverId,
-                    'id' => $this->chatId,
-                    'sessionId' => $this->sessionId,
-                    'status' => $this->userStatus,
-                    'created_at' => $this->created_at,
-                ];
+                $query = AdminGuestChat::where('user_id', $this->user->id)
+                ->where('session_id', $this->sessionId)
+                ->where('admin_id', $this->receiverId)
+                ->get();
+
+                $chatDataArray = [];
+
+                
+                foreach ($query as $chatData) {
+                    
+                    $chatDataArray[] = [
+                        'user_id' => $chatData->user_id,
+                        'message' => $chatData->message,
+                        'image' => $chatData->image ?? null,
+                        'admin_id' => $chatData->admin_id,
+                        'id' => $chatData->id,
+                        'sessionId' => $chatData->session_id,
+                        'status' => $chatData->status,
+                        'created_at' => $chatData->created_at,
+                    ];
+                }
+
+                return $chatDataArray;
             }else {
                 $query = AdminGuestChat::where('user_id', $this->receiverId)
                 ->where('session_id', $this->sessionId)

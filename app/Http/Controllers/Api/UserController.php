302846c65ef2cf85182e284ec54fddf877ee1039
Ebube Uzor <ebubeuzor17@gmail.com>
2024-04-29 11:34:50 +0100

    "Removed nested where clauses for booking security deposit queries in UserController"

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -128,14 +128,9 @@ class UserController extends Controller
             // Retrieve records from bookings where the user is the guest and securityDepositToGuest is not null
             $bookingSecurityDepositToGuestRecords = Booking::join('host_homes', 'bookings.host_home_id', '=', 'host_homes.id')
             ->where('bookings.user_id', $userId)
-            ->where(function ($query) {
-                $query->whereNotNull('bookings.addedToGuestWallet')
-                    ->orWhere(function ($query) {
-                        $query->whereNotNull('bookings.securityDepositToGuest')
-                            ->whereNotNull('bookings.pauseSecurityDepositToGuest');
-                    });
-            })
+            ->whereNotNull('bookings.addedToGuestWallet')
             ->select('bookings.id', 'bookings.securityDeposit as amount', 'bookings.created_at', 'bookings.updated_at')
+            ->selectRaw('host_homes.id AS hosthome_id, host_homes.title AS hosthome_title')
             ->get();
 
 

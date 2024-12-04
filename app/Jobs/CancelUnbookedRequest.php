c86d220934c90b083bc7e0bf3efba176a6e8661b
Ebube Uzor <ebubeuzor17@gmail.com>
2024-12-04 07:37:17 +0100

    "Added use statement for Illuminate\Support\Facades\Mail in CancelUnbookedRequest.php"

--- app/Jobs/CancelUnbookedRequest.php
+++ app/Jobs/CancelUnbookedRequest.php
@@ -11,6 +11,7 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Mail;
 
 class CancelUnbookedRequest implements ShouldQueue
 {

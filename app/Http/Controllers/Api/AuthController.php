dfb4128c26f0a4e22c0156095c492e71211a9035
Ebube Uzor <ebubeuzor17@gmail.com>
2025-02-20 09:22:06 +0100

    `Refactor mobileRequest logic in AuthController::signup method`

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -311,8 +311,7 @@ class AuthController extends Controller
             ]);
         }
 
-        // Fix 1: Convert to boolean and cast to string for URL compatibility
-        $mobileRequest = (!empty($data['mobileRequest'])) ? 'true' : 'false';
+        $mobileRequest = isset($data['mobileRequest']) && $data['mobileRequest'] === true;
 
         info("mobileRequest1=".$mobileRequest);
         Mail::to($user->email)->queue(
